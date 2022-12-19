0ecf444eb5cb1c7b2d2a44f5205a061429a2401f
guicalheira <guicalheira14@gmail.com>
2022-12-19 00:54:52 -0300

    GrandeUpdate

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Button.css";
+
+export default props => {
+  let classes = 'button '
+classes += props.double ? "double" : " "
+classes += props.triple ? "triple" : " "
+classes += props.operation ? "operation" : " "
+   
+  return (
+      <button
+        onClick={(e) => props.click && props.click(props.label)}
+        className={classes}>{props.label}</button>
+  );
+};
